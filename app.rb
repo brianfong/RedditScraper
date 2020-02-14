2b3081e296ae4fbe7d870b2297852056b62f9f6c
brianfong <brianjonathanfong@gmail.com>
2020-02-13 20:39:26 -0800

    cleanup

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require 'pg'
+#require 'pg'
 require 'shotgun'
 require 'faraday'
 require 'json'
@@ -9,6 +9,7 @@ require 'logger'
 require 'uri'
 require 'fileutils'
 require 'mini_magick'
+require 'sqlite3'
 
 logger = Logger.new(STDOUT)
 logger.level = Logger::INFO
@@ -17,32 +18,17 @@ def user_agent
   "Reddit::Scraper v0.0.4 (https://github.com/brianfong/RedditScraper)"
 end
 
-conn = PG.connect.new(dbname: 'development.pg')
-  # Ensures the table is created if it doesn't exist
-  conn.exec("CREATE TABLE IF NOT EXISTS memes (
-    name varchar, 
-    author text,
-    title text,
-    url text,
-    permalnk text
-    );")
-  res.result_status
-rescue PG::Error => pg_error
-  puts "Table creation failed: #{pg_error.message}"
-end
+db = SQLite3::Database.new "test.db"
 
-
-# db = SQLite3::Database.new "test.db"
-
-# rows = db.execute <<-SQL
-# create table if not exists posts (
-#   name varchar(30),
-#   author text,
-#   title text,
-#   url text,
-#   permalink text
-#  );
-#=SQL
+rows = db.execute <<-SQL
+create table if not exists posts (
+  name varchar(30),
+  author text,
+  title text,
+  url text,
+  permalink text
+ );
+=SQL
 
 rows = db.execute <<-SQL
 create unique index if not exists name_posts on posts(name);
@@ -52,11 +38,9 @@ Faraday.default_connection = Faraday.new(options = {:headers=>{:user_agent => us
 
 conn = Faraday.default_connection
 
-# TODO: Once you have this all in the database, you can then fetch with query params before_id/after_id depending on how you want to loop
-response = conn.get 'https://www.reddit.com/r/memes/.json?limit=10'
+response = conn.get 'https://www.reddit.com/r/thumbcats/.json?limit=100'
 parsed_json = JSON.parse(response.body.to_json)
 
-# When in debug; write out the full thing; otherwise we'll skip it
 logger.debug parsed_json
 
 JSON.parse(parsed_json)['data']['children'].each do |child|
@@ -68,7 +52,6 @@ JSON.parse(parsed_json)['data']['children'].each do |child|
 
   #binding.pry
 
-  #       Write some migrations, use ActiveRecord.  
   def existsCheck(permalink)
     temp = db.execute( "SELECT 1 where exists(
         SELECT permalink
@@ -90,7 +73,7 @@ JSON.parse(parsed_json)['data']['children'].each do |child|
     image = MiniMagick::Image.open("#{url}")
     # binding.pry
     logger.info "Exif: #{image.exif}"
-    image.resize "500x500"
+    #image.resize "500x500"
     image.format "png"
     image.write "./tmp/#{name}.png"
 
